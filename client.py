4b7aa010b35c35f83b2b26b2c1d56fa62632a3a9
varshateratipally <t.varsha016@gmail.com>
2018-04-26 06:13:26 -0600

    Update client.py

--- client.py
+++ client.py
@@ -1,65 +1,72 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+from CoreLocation import (CLLocationManager,
+                          CLLocation,
+                          kCLDistanceFilterNone,
+                          kCLLocationAccuracyBest)
+from Foundation import (NSRunLoop,
+                        NSDate,
+                        NSObject,
+                        NSBundle)
 from suds.client import Client
-import geocoder
-import time
-i=0
-while(i<10):
-    time.sleep(10)
-    g = geocoder.ip('me')
-    c = Client('http://localhost:8030/?wsdl')
-    c.service.say_hello('Macbookair', str(g.latlng[0])+','+str(g.latlng[1]))
-    i = i+1
 
+ 
+c = Client('http://localhost:8030/?wsdl')
+class LocationClient(NSObject):
 
-##import boto3
-##from boto3.dynamodb.conditions import Key, Attr
-##dynamodb = boto3.resource('dynamodb',
-##                          aws_access_key_id='AKIAIGEWAQQRTVXDW3ZA   aws_secret_access_key='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'
-##                          ,region_name='us-east-2')
-##dynamodb_client = boto3.client('dynamodb', aws_access_key_id = 'AKIAIGEWAQQRTVXDW3ZA',
-##                        aws_secret_access_key = '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',
-##                        region_name = 'us-east-2')
-##
-##table = dynamodb.Table(name=u'ItemTracker_tbl')
-##response  = table.query(KeyConditionExpression=Key('itemtrackerId').eq("ABC123"))
-##print(response)
-##
+    is_authorized = CLLocationManager.authorizationStatus()
+    is_enabled = CLLocationManager.locationServicesEnabled()
+ 
+    def init(self):
+        """Define location manager settings for lookups."""
+        self = super(LocationClient, self).init()
+        if not self:
+            return
+        self.locationManager = CLLocationManager.alloc().init()
+        self.locationManager.setDelegate_(self)
+        self.locationManager.setDistanceFilter_(kCLDistanceFilterNone)
+        self.locationManager.setDesiredAccuracy_(kCLLocationAccuracyBest)
+        self.locationManager.startUpdatingLocation()
+        return self
+ 
+    def locationManager_didUpdateToLocation_fromLocation_(self, manager,
+                                                          newloc, oldloc):
+        """Splits location data into separate pieces for processing later."""
+        lat = newloc.coordinate().latitude
+        lon = newloc.coordinate().longitude
+        verAcc = newloc.verticalAccuracy()
+        horAcc = newloc.horizontalAccuracy()
+        altitude = newloc.altitude()
+        time = newloc.timestamp()
+        gmap = (r"https://www.google.com/maps/place/{0},{1}"
+                "/@{0},{1},18z/data=!3m1!1e3".format(str(lat), str(lon)))
+        plist = dict(
+            Altitude=int(altitude),
+            CurrentStatus=str("Successful"),
+            GoogleMap=str(gmap),
+            LastLocationRun=str(time),
+            Latitude=str(lat),
+            LatitudeAccuracy=int(verAcc),
+            Longitude=str(lon),
+            LongitudeAccuracy=int(horAcc),
+        )
+        c.service.say_hello('ABC234', str(plist['Latitude'])+','+str(plist['Longitude']))
 
-##
-##import boto3
-##from boto3.dynamodb.conditions import Key, Attr
-##dynamodb = boto3.resource('dynamodb', aws_access_key_id = 'AKIAI7HHEQFZZ6KQ536A',
-##                        aws_secret_access_key = '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',
-##                        region_name = 'us-east-2')
-##dynamodb_client = boto3.client('dynamodb', aws_access_key_id = 'AKIAI7HHEQFZZ6KQ536A',
-##                        aws_secret_access_key = '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',
-##                        region_name = 'us-east-2')
-##
-##existing_tables = dynamodb_client.list_tables()['TableNames']
-##print(existing_tables)
-##table_name = 'ItemTracker_tbl'
-##itemtrackerId = 'ABC123'
-##if table_name not in existing_tables:
-##    table = dynamodb.create_table(TableName=table_name,
-##                                  KeySchema=[{'AttributeName': 'itemtrackerId'
-##                                              , 'KeyType': 'HASH'}],
-##                                  AttributeDefinitions=[{'AttributeName': 'itemtrackerId'
-##                                                         , 'AttributeType': 'S'}],
-##                                  ProvisionedThroughput={'ReadCapacityUnits': 5,
-##                                                         'WriteCapacityUnits': 5})
-##
-##    # Wait until the table exists.
-##    table.meta.client.get_waiter('table_exists').wait(TableName='ItemTracker_tbl')
-##
-##table = dynamodb.Table('ItemTracker_tbl')
-##
-##table.put_item(Item = { 'itemtrackerId':itemtrackerId,'lat' : ['12.23'], 'lon':['11.23'] })
-##table.update_item(Key={'itemtrackerId': itemtrackerId},
-##                              UpdateExpression='SET lat = list_append(lat, :i)'
-##                              , ExpressionAttributeValues={':i': ['12.3']},
-##                              ReturnValues='UPDATED_NEW')
-##response  = table.query(KeyConditionExpression=Key('itemtrackerId').eq('123')
-##)
-##
-##
+        print("Successful lookup request: {0}, {1}".format(
+              plist['Latitude'], plist['Longitude']))
+ 
+    def locationManager_didFailWithError_(self, manager, err):
+        """Handlers errors for location manager."""
+        if self.is_enabled is True:
+            if self.is_authorized == 3:
+                status = "Unable to locate"
+            if self.is_authorized == 2:
+                status = "Denied"
+            if self.is_authorized == 1:
+                status = "Restricted"
+            if self.is_authorized == 0:
+                status = "Not Determined"
+        else:
+            status = "Location Services Disabled"
+        print(status)
+ 
+finder = LocationClient.alloc().init()
+NSRunLoop.currentRunLoop().runUntilDate_(NSDate.dateWithTimeIntervalSinceNow_(5))
