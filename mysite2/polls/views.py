420cb925f2a696c3c7b691577962c44d758ab9b6
Ganesh <ganesh.byrandurgagopinath@colorado.edu>
2018-04-27 16:42:24 -0600

    Update views.py

--- mysite2/polls/views.py
+++ mysite2/polls/views.py
@@ -21,8 +21,8 @@ def main(request):
     try:
 	id_1 = request.POST.get('itid', 'N/A')
 	dynamodb = boto3.resource('dynamodb',
-                          	aws_access_key_id='AKIAJH7QCO7VUC43PFYQ',
-                                aws_secret_access_key='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'
+                          	aws_access_key_id='',
+                                aws_secret_access_key=''
                               , region_name='us-east-2')
         table = dynamodb.Table(name=u'ItemTracker_tbl')
         response = table.get_item(Key = {'itemtrackerId':id_1})
@@ -38,13 +38,13 @@ def main(request):
 	template = loader.get_template('polls/error.html')
         return HttpResponse(template.render(context,request))
 
-def req():
+'''def req():
         client = boto3.resource(
         'dynamodb',
         aws_access_key_id='',
         aws_secret_access_key='',
         )
-        return client
+        return client'''
 
 @csrf_exempt
 def test(request):
@@ -67,28 +67,3 @@ def index(request):
         template=loader.get_template('polls/index.html')
         return HttpResponse(template.render(context,request))
 
-def receiver(request):
-        data_write=defaultdict(lambda:{})
-        if request.method=="POST":
-                Id=request.POST.get('Id',"N/A")
-                latitude=request.POST.get('latitude',"N/A")
-                longitude=request.POST.get('longitude',"N/A")
-                with open('details.json','r') as det:
-                        data=json.load(det)
-                        data_write=data
-                        if Id in data_write.keys():
-                                data_write[Id]["latitude"].append(latitude)
-                                data_write[Id]["longitude"].append(longitude)
-                        else:
-                                data_write[Id]={}
-                                data_write[Id]["latitude"]=[]
-                                data_write[Id]["longitude"]=[]
-                                data_write[Id]["latitude"].append(latitude)
-                                data_write[Id]["longitude"].append(longitude)
-                        data_write=data
-                with open('details.json','w') as details_write:
-                        json.dump(data_write,details_write)
-                context ={'ad'}
-        template=loader.get_template('polls/results.html')
-        
-        return HttpResponse(template.render(context,request))
